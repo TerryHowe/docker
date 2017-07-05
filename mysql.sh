756b800b4d87ad7866d30646053c7e5f5c92c4d4
Terry Howe <terrylhowe@gmail.com>
2017-07-05 11:07:46 -0600

    add mysql

---
+++ mysql.sh
@@ -0,0 +1,4 @@
+PERSISTENT_DIRECTORY="$HOME/persistent/mysql"
+mkdir -p $PERSISTENT_DIRECTORY
+docker run --name mysql -v ${PERSISTENT_DIRECTORY}:/var/lib/mysql -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always mysql:5.7
+
