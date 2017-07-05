da33375210ca18bff83db6f6e9d63430ef2e99c8
Terry Howe <terrylhowe@gmail.com>
2017-07-05 09:45:55 -0600

    fix mkdir opt

--- redis.sh
+++ redis.sh
@@ -1,4 +1,4 @@
 PERSISTENT_DIRECTORY="$HOME/persistent/redis"
-mkdir -f $PERSISTENT_DIRECTORY
-docker run --name terry-redis -v ${PERSISTENT_DIRECTORY}:/data -d --restart always redis:3.2
+mkdir -p $PERSISTENT_DIRECTORY
+docker run --name redis -v ${PERSISTENT_DIRECTORY}:/data -d --restart always redis:3.2
 
