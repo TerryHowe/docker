91ae873ed150cd06dc93a8b05922c34f432e82d3
Terry Howe <terrylhowe@gmail.com>
2017-07-12 13:54:48 -0600

    get rids working on mac with port on localhost

--- redis.sh
+++ redis.sh
@@ -1,4 +1,4 @@
 PERSISTENT_DIRECTORY="$HOME/persistent/redis"
 mkdir -p $PERSISTENT_DIRECTORY
-docker run --name redis -h redis -v ${PERSISTENT_DIRECTORY}:/data -d --restart always redis:3.2
+docker run --name redis -h redis -v ${PERSISTENT_DIRECTORY}:/data -p 127.0.0.1:6379:6379 -d --restart always redis:3.2
 
