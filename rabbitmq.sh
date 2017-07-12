6740e66f61bbc2f07e48d4542ce7dc5779244f69
Terry Howe <terrylhowe@gmail.com>
2017-07-12 13:16:59 -0600

    rabbit for mac where bridge network broken

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,4 +1,3 @@
 PERSISTENT_DIRECTORY="$HOME/persistent/rabbitmq"
 mkdir -p $PERSISTENT_DIRECTORY
-docker run --name rabbitmq -h rabbitmq -v ${PERSISTENT_DIRECTORY}:/var/lib/rabbitmq -d --restart always rabbitmq:3.6
-
+docker run --name rabbitmq -h rabbitmq -v ${PERSISTENT_DIRECTORY}:/var/lib/rabbitmq -p 127.0.0.1:5672:5672 -d --restart always rabbitmq:3.6
