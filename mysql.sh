7008cb07338806c071dbd9ca728d2b877a1690b2
Terry Howe <terrylhowe@gmail.com>
2017-07-14 12:34:19 -0600

    bind the port to localhost

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
 PERSISTENT_DIRECTORY="$HOME/persistent/mysql"
 mkdir -p $PERSISTENT_DIRECTORY
-docker run --name mysql -h mysql -v ${PERSISTENT_DIRECTORY}:/var/lib/mysql -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always mysql:5.7
+docker run --name mysql -h mysql -v ${PERSISTENT_DIRECTORY}:/var/lib/mysql  -p 127.0.0.1:3306:3306 -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -d --restart always mysql:5.7
 
