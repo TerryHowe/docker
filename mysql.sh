f3f51be508032adb5a09600caefbaf32c49736d5
Terry Howe <terrylhowe@gmail.com>
2018-10-09 11:08:43 -0600

    mysql cnf

--- mysql.sh
+++ mysql.sh
@@ -2,12 +2,16 @@
 #docker rm mysql
 PERSISTENT_DIRECTORY="$HOME/persistent/mysql"
 mkdir -p $PERSISTENT_DIRECTORY
-#cat >${PERSISTENT_DIRECTORY}/my.cnf <<!
-#explicit_defaults_for_timestamp = 1
-#!
-# -v ${PERSISTENT_DIRECTORY}/my.cnf:/etc/mysql/conf.d/mysql.cnf \
+cat >${PERSISTENT_DIRECTORY}/my.cnf <<!
+[mysqld]
+skip-host-cache
+skip-name-resolve
+sql_mode = NO_ENGINE_SUBSTITUTION
+explicit_defaults_for_timestamp = 1
+!
 docker run --name mysql -h mysql \
     -v ${PERSISTENT_DIRECTORY}:/var/lib/mysql \
+    -v ${PERSISTENT_DIRECTORY}/my.cnf:/etc/mysql/conf.d/docker.cnf \
     -e "MYSQL_ALLOW_EMPTY_PASSWORD=yes" \
     -p 127.0.0.1:3306:3306 \
     -d \
